6b0907b8a4d07980a94c4ecbcda1c8cbe8a9800d
Adam <adam.gonzalez@zoho.com>
2019-02-20 11:10:08 -0800

    Updated main to work

--- main.cpp
+++ main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
+//using std::cout;
+//using std::cin;
+//using std::endl;
 
-void sortDescending(int,int,int);
+using namespace std;
+
+void sortDescending(int&,int&,int&);
 void swap(int&,int&);
 
 int main()
 {
   int numA, numB, numC;
-  cout<<"Enter any three numbers: ";
-  cin>>numA>>numB>>numC;
+  cout << "Enter any three numbers: ";
+  cin >> numA >> numB >> numC;
 
   sortDescending(numA, numB, numC);
   
-  cout<<"From greatest to least, they are: ";
-  cout<<numA<<","<<numB<<","<<numC<<endl;
+  cout << "From greatest to least, they are: ";
+  cout << numA << "," << numB << "," << numC <<endl;
   return 0;
 }
 
-void sortDescending(int first, int second, int third)
+void sortDescending(int &first, int &second, int &third)
 {
   if( first < third )
   {
